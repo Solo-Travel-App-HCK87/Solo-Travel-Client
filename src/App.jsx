535695dca792cc302af6bcc6fbd65f12247ac6e6
Gerry0303 <imanuelgerry0303@gmail.com>
2025-09-23 14:52:25 +0700

    feat: add PackageCard and PackagePage components with package listing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import { MainLayout } from './layouts/MainLayout';
+import PackagePage from './pages/PackagePage';
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/package" element={<PackagePage />} />
         </Route>
+        <Route path="/package/:id" element={<h1>Package Details</h1>} />
       </Routes>
     </BrowserRouter>
   );
