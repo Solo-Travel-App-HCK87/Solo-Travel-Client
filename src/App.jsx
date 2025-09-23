bb20866947d03262d9a9d400853d6223074cfbb7
harryrismananda <harryrismananda@gmail.com>
2025-09-23 15:39:48 +0700

    feat: Register page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import { MainLayout } from './layouts/MainLayout';
-import PackagePage from './pages/PackagePage';
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import { MainLayout } from "./layouts/MainLayout";
+import PackagePage from "./pages/PackagePage";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/package" element={<PackagePage />} />
