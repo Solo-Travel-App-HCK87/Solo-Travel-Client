86c63d4edb1b93b483c47cf5bcf079b41e749503
Gerry0303 <imanuelgerry0303@gmail.com>
2025-09-23 13:00:42 +0700

    Add Navbar component and MainLayout for routing structure
    - Implemented Navbar component with logo and navigation links.
    - Added responsive design for navigation items.
    - Integrated Users icon from lucide-react for user-related actions.
    - Created MainLayout to wrap the Navbar and render child routes using Outlet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
+import { MainLayout } from './layouts/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
